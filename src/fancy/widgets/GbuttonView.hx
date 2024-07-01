c25f1c2b4d67a49e55228b27adcc7aa8734394bd
artem pecheny <pecheny@gmail.com>
2024-07-02 01:43:14 +0300

    GbuttonView

---
+++ src/fancy/widgets/GbuttonView.hx
@@ -0,0 +1,41 @@
+package fancy.widgets;
+
+import al.al2d.Placeholder2D;
+import al.al2d.Widget;
+import ec.CtxWatcher;
+import ginp.GameButtons;
+import shimp.ClicksInputSystem.ClickTargetViewState;
+import update.Updatable;
+import update.UpdateBinder;
+import widgets.ButtonBase.ClickViewProcessor;
+
+class GbuttonView<TB:Axis<TB>> extends Widget implements Updatable implements ClickViewProcessor {
+    var interactives:Array<ClickTargetViewState->Void> = [];
+    var lastState:Bool;
+    var gameButton:TB;
+    @:once var inp:GameButtons<TB>;
+
+    public function new(ph:Placeholder2D, gb:TB) {
+        super(ph);
+        gameButton = gb;
+    }
+
+    public function addHandler(h:ClickTargetViewState->Void) {
+        interactives.push(h);
+    }
+
+    public function update(dt:Float) {
+        var val = (inp.pressed(gameButton));
+        if (val == lastState)
+            return;
+        lastState = val;
+        for (h in interactives)
+            h(val ? Pressed : Idle);
+    }
+
+    override function init() {
+        ph.entity.addComponentByType(Updatable, this);
+        new CtxWatcher(UpdateBinder, entity);
+        super.init();
+    }
+}
