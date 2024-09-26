76a8569c80f2c5d74c8fafad0011cc970801b45f
artem pecheny <pecheny@gmail.com>
2024-09-26 17:22:27 +0300

    Fix DkitDemo.

--- demo/domkit/src/DkitDemo.hx
+++ demo/domkit/src/DkitDemo.hx
@@ -1,17 +1,16 @@
 package;
 
-import fu.PropStorage;
-import a2d.Layouts.ContainerStyler;
+import a2d.ContainerStyler;
+import al.layouts.PortionLayout;
 import al.layouts.WholefillLayout;
 import al.layouts.data.LayoutData;
-import al.layouts.PortionLayout;
 import fancy.WidgetTester;
 import fancy.domkit.Dkit;
+import fu.PropStorage;
 
+using a2d.transform.LiquidTransformer;
+using a2d.transform.LiquidTransformer;
 using al.Builder;
-using a2d.transform.LiquidTransformer;
-using a2d.transform.LiquidTransformer;
-
 
 class DkitDemo extends WidgetTester {
     public function new() {
@@ -27,7 +26,7 @@ class DkitDemo extends WidgetTester {
     function createStyles() {
         var default_text_style = "small-text";
 
-        var pcStyle = fui.textStyles.newStyle(default_text_style) 
+        var pcStyle = fui.textStyles.newStyle(default_text_style)
             .withSize(sfr, .07)
             .withPadding(horizontal, sfr, 0.1)
             .withAlign(vertical, Center)
@@ -41,7 +40,6 @@ class DkitDemo extends WidgetTester {
         var contLayouts = new ContainerStyler();
         contLayouts.reg(GuiStyles.L_HOR_CARDS, distributer, WholefillLayout.instance);
         e.addComponent(contLayouts);
-
     }
 }
 
@@ -51,12 +49,13 @@ class DomkitSampleWidget extends BaseDkit {
         
         <label(b().v(pfr, .2).b()) id="lbl"  text={ "Do you want to buy smth?" }  >
         </label>
-        <base(b().v(pfr, 0.7).b()) id="cardsContainer"  layouts={GuiStyles.L_HOR_CARDS} >
+        <base(b().v(pfr, 0.7).b())  >
         ${fui.quad(__this__.ph, 0xff025f29)}
         </base>
-        <button(b().v(pfr, .1).b())   text={ "Ok" } onClick={onOkClick}  />
+        <button(b().v(pfr, .1).h(sfr, 0.5).b())   text={ "Ok" } onClick={onOkClick}  />
+        <base(b().v(pfr, 0.2).b()) />
     </domkit-sample-widget>
-    
+
     function onOkClick() {
         trace("click");
     }
