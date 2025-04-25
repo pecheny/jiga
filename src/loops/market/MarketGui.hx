2c47b3f5b245a36cc1e90ab62a35eb8a9c81fc31
artem pecheny <pecheny@gmail.com>
2025-04-25 10:56:29 +0300

    MarketGui appearance tuning.

--- src/loops/market/MarketGui.hx
+++ src/loops/market/MarketGui.hx
@@ -1,47 +1,44 @@
 package loops.market;
 
-import loops.market.MarketActivity.MarketGui;
-import dkit.Dkit.BaseDkit;
-import a2d.ChildrenPool;
+import htext.Align;
 import a2d.Placeholder2D;
 import al.core.DataView;
 import al.layouts.PortionLayout;
-import fu.bootstrap.ButtonScale;
-import fu.ui.InteractivePanelBuilder;
-import fu.ui.ButtonEnabled;
+import dkit.Dkit.BaseDkit;
 import fu.Signal.IntSignal;
-import fu.ui.Button;
+import fu.bootstrap.ButtonScale;
+import fu.ui.ButtonEnabled;
 import fu.ui.Properties;
+import loops.market.MarketActivity.MarketGui;
 import loops.market.MarketData;
-import utils.Signal;
 
-using al.Builder;
 using a2d.ProxyWidgetTransform;
+using al.Builder;
 
 class MarketWidget extends BaseDkit implements MarketGui {
     public var onDone:ec.Signal<Void->Void> = new ec.Signal();
     public var onChoice(default, null) = new IntSignal();
 
     static var SRC = <market-widget vl={PortionLayout.instance}>
-        <label(b().v(pfr, .2).b()) id="lbl"  text={ "Do you want to buy smth?" }  >
-        </label>
-        <base(b().v(pfr, 0.7).b()) id="cardsContainer"  layouts={GuiStyles.L_HOR_CARDS} />
-        <button(b().v(pfr, .1).b())   text={ "Ok" } onClick={onOkClick}  />
+        <label(b().v(pfr, .24).b()) id="lbl"  text={ "Do you want to buy smth?" } align={Align.Center}/>
+        <base(b().v(pfr, .24).b()) />
+
+        <data-container(b().v(pfr, 1).b()) id="cardsContainer"   itemFactory={cardFactory} inputFactory={inputFactory} layouts={GuiStyles.L_HOR_CARDS }  />
+
+        <base(b().v(pfr, .24).b()) />
+        <button(b().h(sfr, .36).v(sfr, .12).b())   text={ "Done" } onClick={onOkClick} style={"center"} />
     </market-widget>;
 
-    var buttons:DataChildrenPool<MarketItemRecord, MarketCard>;
     var maxNumber:Int;
 
-    override function init() {
-        buttons = new InteractivePanelBuilder<MarketItemRecord, MarketCard>().withContainer(cardsContainer.c)
-            .withWidget(() -> {
-                var mc = new MarketCard(b().v(sfr, 0.3).h(sfr, 0.3).t(1).b("card"));
-                new ButtonScale(mc.entity);
-                mc;
-            })
-            .withSignal(onChoice)
-            .withInput((ph, n) -> new ButtonEnabled(ph, onChoice.dispatch.bind(n)))
-            .build();
+    function cardFactory() {
+        var mc = new MarketCard(b().v(sfr, 0.3).h(sfr, 0.3).t(1).b("card"));
+        new ButtonScale(mc.entity);
+        return mc;
+    }
+
+    function inputFactory(ph, n) {
+        new ButtonEnabled(ph, onChoice.dispatch.bind(n));
     }
 
     function onOkClick() {
@@ -49,7 +46,7 @@ class MarketWidget extends BaseDkit implements MarketGui {
     }
 
     public function initData(items:Array<MarketItemRecord>) {
-        buttons.initData(items);
+        cardsContainer.initData(items);
     }
 }
 
@@ -59,10 +56,10 @@ class MarketCard extends BaseDkit implements DataView<MarketItemRecord> {
 
     static var SRC = <market-card vl={PortionLayout.instance} >
         <switcher(b().v(pfr, 1).b()) id="content"   >
-            <label(b().v(pfr, 0.2).b()) id="soldCard" text={"X"}  />
-            <label(b().v(pfr, 0.2).b()) id="card"   />
+            <label(b().v(pfr, 0.2).b()) id="soldCard" text={"X"} align={Align.Center} />
+            <label(b().v(pfr, 0.2).b()) public id="card"   />
         </switcher>
-        <label(b().v(pfr, 0.2).b()) id="lbl"   />
+        <label(b().v(pfr, 0.2).b()) id="lbl" align={Align.Center}  />
     </market-card>;
 
     public function initData(descr:MarketItemRecord) {
@@ -75,6 +72,7 @@ class MarketCard extends BaseDkit implements DataView<MarketItemRecord> {
 
     function onChange(s:MarketItemState) {
         toggle.enabled = s == MarketItemState.available;
+        lbl.color = s == MarketItemState.na ? 0xff0000 : 0xffffff;
         if (s == sold) {
             lbl.text = "Sold out!";
             content.switchTo(soldCard.ph);
