bc430b384681ebdaac856108c26f666f0c4da821
artem pecheny <pecheny@gmail.com>
2025-05-25 17:25:30 +0300

    BootstrapMain.regTextProcessor() to reg custom preprocessors.

--- src/bootstrap/BootstrapMain.hx
+++ src/bootstrap/BootstrapMain.hx
@@ -46,162 +46,165 @@ using a2d.transform.LiquidTransformer;
 using al.Builder;
 
 class BootstrapMain extends AbstractEngine {
-	var fui = new FuiBuilder();
-	var rootEntity:Entity;
-	var stateSwitcher:StateSwitcher;
+    var fui = new FuiBuilder();
+    var rootEntity:Entity;
+    var stateSwitcher:StateSwitcher;
 
-	public function new() {
-		super();
-		var root = rootEntity = new Entity("root");
-		setWindowPosition();
-		// regDrawcals();
+    public function new() {
+        super();
+        var root = rootEntity = new Entity("root");
+        setWindowPosition();
+        // regDrawcals();
+
+        regTextProcessor();
         var uikit = new Uikit(fui);
         uikit.configure(root);
         uikit.createContainer(root);
         root.addComponentByType(fu.Uikit, uikit);
 
+        textStyles();
+        createFlashDisplay();
+        initFui();
+        dkitDefaultStyles();
+        createInput();
+        iniUpdater();
 
-		textStyles();
-		createFlashDisplay();
-		initFui();
-		dkitDefaultStyles();
-		createInput();
-		iniUpdater();
+        // rootEntity.dispatchContext(rootEntity);
+        createRunWrapper();
+        createRun();
+    }
 
-		// rootEntity.dispatchContext(rootEntity);
-		createRunWrapper();
-		createRun();
-	}
+    function regTextProcessor() {}
 
-	function createRun() {}
+    function createRun() {}
 
-	function enterRun(run:GameRun) {
-		run.entity.addComponentByType(GameRun, run);
-		new CtxWatcher(GameRunBinder, run.entity);
-		rootEntity.addChild(run.entity);
-	}
+    function enterRun(run:GameRun) {
+        run.entity.addComponentByType(GameRun, run);
+        new CtxWatcher(GameRunBinder, run.entity);
+        rootEntity.addChild(run.entity);
+    }
 
-	function createRunWrapper() {
-		new SimpleRunBinder(rootEntity, null);
-	}
+    function createRunWrapper() {
+        new SimpleRunBinder(rootEntity, null);
+    }
 
-	function regDrawcals() {
-		// var pipeline = fui.pipeline;
-		// pipeline.addPass(GuiDrawcalls.BG_DRAWCALL, new FlatColorPass());
-		// pipeline.addPass(GuiDrawcalls.TEXT_DRAWCALL, new CmsdfPass());
-		// var fontAsp = new FontAspectsFactory(fui.fonts, pipeline.textureStorage);
-		// pipeline.addAspectExtractor(GuiDrawcalls.TEXT_DRAWCALL, fontAsp.create, fontAsp.getAlias);
+    function regDrawcals() {
+        // var pipeline = fui.pipeline;
+        // pipeline.addPass(GuiDrawcalls.BG_DRAWCALL, new FlatColorPass());
+        // pipeline.addPass(GuiDrawcalls.TEXT_DRAWCALL, new CmsdfPass());
+        // var fontAsp = new FontAspectsFactory(fui.fonts, pipeline.textureStorage);
+        // pipeline.addAspectExtractor(GuiDrawcalls.TEXT_DRAWCALL, fontAsp.create, fontAsp.getAlias);
 
-		// pipeline.addPass(PictureDrawcalls.IMAGE_DRAWCALL, new ImagePass());
-		// var picAsp = new TextureAspectFactory(pipeline.textureStorage);
-		// pipeline.addAspectExtractor(PictureDrawcalls.IMAGE_DRAWCALL, picAsp.create);
-	}
+        // pipeline.addPass(PictureDrawcalls.IMAGE_DRAWCALL, new ImagePass());
+        // var picAsp = new TextureAspectFactory(pipeline.textureStorage);
+        // pipeline.addAspectExtractor(PictureDrawcalls.IMAGE_DRAWCALL, picAsp.create);
+    }
 
-	function setWindowPosition() {
-		var wnd = openfl.Lib.application.window;
-		if (wnd.y < 0)
-			wnd.y = 20;
-		wnd.x = 200;
-	}
+    function setWindowPosition() {
+        var wnd = openfl.Lib.application.window;
+        if (wnd.y < 0)
+            wnd.y = 20;
+        wnd.x = 200;
+    }
 
-	/**
-		Creates root for multitouch input targets.
-	**/
-	function createMultiInput() {
-		var ar = fui.ar;
-		var mibinder = new MultiInputBinder();
-		var mroot = new MultitouchRoot(mibinder, ar.getAspectRatio());
-		var root = new InputRoot(new InputToMultiTarget(mibinder, -1), ar.getAspectRatio());
-		rootEntity.addComponent(mibinder);
-	}
+    /**
+        Creates root for multitouch input targets.
+    **/
+    function createMultiInput() {
+        var ar = fui.ar;
+        var mibinder = new MultiInputBinder();
+        var mroot = new MultitouchRoot(mibinder, ar.getAspectRatio());
+        var root = new InputRoot(new InputToMultiTarget(mibinder, -1), ar.getAspectRatio());
+        rootEntity.addComponent(mibinder);
+    }
 
-	/**
-		Creates and binds GameInput. By default it is OneButton, supposed to override for exact buttons and axis set. 
-	**/
-	function createInput() {
+    /**
+        Creates and binds GameInput. By default it is OneButton, supposed to override for exact buttons and axis set. 
+    **/
+    function createInput() {
         var kbd = rootEntity.addComponentByType(ginp.api.KbdDispatcher, new openfl.OflKbd());
-		var input = new OneButtonInput();
+        var input = new OneButtonInput();
         var gk = input.createKeyMapping([ginp.Keyboard.SPACE => OneButton.button]);
         kbd.addListener(gk);
 
-		rootEntity.addComponentByType(GameInputUpdater, input);
-		rootEntity.addComponentByType(GameButtons, input);
-		rootEntity.addComponent(input);
-		// listen for gui buttons dispatchers
-		rootEntity.addComponentByName(MGA.toAlias(ButtonInputBinder, OneButton), new ButtonInputBinder(MGA.toString(OneButton), input.buttonListener));
-	}
+        rootEntity.addComponentByType(GameInputUpdater, input);
+        rootEntity.addComponentByType(GameButtons, input);
+        rootEntity.addComponent(input);
+        // listen for gui buttons dispatchers
+        rootEntity.addComponentByName(MGA.toAlias(ButtonInputBinder, OneButton), new ButtonInputBinder(MGA.toString(OneButton), input.buttonListener));
+    }
 
-	function createGameplaySimple() {
-		var entity = new Entity("run");
-		var fac = new MyWeaponFac();
-		entity.addComponent(fac.createDummyTracer());
-		var bouncing = new BouncingWidget(Builder.widget());
-		var loop = new BouncingLoop(entity, bouncing.ph, bouncing);
-		return entity;
-	}
+    function createGameplaySimple() {
+        var entity = new Entity("run");
+        var fac = new MyWeaponFac();
+        entity.addComponent(fac.createDummyTracer());
+        var bouncing = new BouncingWidget(Builder.widget());
+        var loop = new BouncingLoop(entity, bouncing.ph, bouncing);
+        return entity;
+    }
 
-	function createFlashDisplay() {
-		var flashCanvas = new Sprite();
-		addChild(flashCanvas);
-		rootEntity.addComponent(new FlashDisplayRoot(flashCanvas));
-	}
+    function createFlashDisplay() {
+        var flashCanvas = new Sprite();
+        addChild(flashCanvas);
+        rootEntity.addComponent(new FlashDisplayRoot(flashCanvas));
+    }
 
-	function initFui() {
-		var rw = Builder.ph();
-		rootEntity.addComponentByType(Placeholder2D, rw);
-		fui.configureInput(rootEntity);
-		fui.configureScreen(rootEntity);
-		fui.configureAnimation(rootEntity);
-		rootEntity.addComponent(fui);
-		// fui.createContainer(rootEntity, Xml.parse(GuiDrawcalls.DRAWCALLS_LAYOUT).firstElement());
-		// var container:Sprite = rootEntity.getComponent(Sprite);
-		// addChild(container);
-		var v = new StageAspectResizer(rw, 2);
-		var switcher = new WidgetSwitcher(rw);
-		rootEntity.addComponent(switcher);
-	}
+    function initFui() {
+        var rw = Builder.ph();
+        rootEntity.addComponentByType(Placeholder2D, rw);
+        fui.configureInput(rootEntity);
+        fui.configureScreen(rootEntity);
+        fui.configureAnimation(rootEntity);
+        rootEntity.addComponent(fui);
+        // fui.createContainer(rootEntity, Xml.parse(GuiDrawcalls.DRAWCALLS_LAYOUT).firstElement());
+        // var container:Sprite = rootEntity.getComponent(Sprite);
+        // addChild(container);
+        var v = new StageAspectResizer(rw, 2);
+        var switcher = new WidgetSwitcher(rw);
+        rootEntity.addComponent(switcher);
+    }
 
-	function iniUpdater() {
-		var updater = new RunUpdater();
-		addUpdatable(updater);
-		rootEntity.addComponentByType(GameInputUpdaterBinder, updater);
-		rootEntity.addComponent(new UpdateBinder(updater));
-	}
+    function iniUpdater() {
+        var updater = new RunUpdater();
+        addUpdatable(updater);
+        rootEntity.addComponentByType(GameInputUpdaterBinder, updater);
+        rootEntity.addComponent(new UpdateBinder(updater));
+    }
 
-	function textStyles() {
-		// var font = "Assets/fonts/robo.fnt";
-		// fui.addBmFont("", font); // todo
-		var ts = fui.textStyles;
-		ts.newStyle("small-text")
-			.withSize(sfr, .07)
-			.withPadding(horizontal, sfr, 0.1)
-			.withAlign(vertical, Center)
-			.build();
-		ts.resetToDefaults();
-		ts.newStyle("center").withAlign(horizontal, Center).build();
-		ts.newStyle(TextContextBuilder.DEFAULT_STYLE).withAlign(horizontal, Center).build();
-		ts.newStyle("fit")
-			.withSize(pfr, .5)
-			.withAlign(horizontal, Forward)
-			.withAlign(vertical, Backward)
-			.withPadding(horizontal, pfr, 0.33)
-			.withPadding(vertical, pfr, 0.33)
-			.build();
-		rootEntity.addComponent(ts.getStyle("fit"));
-		ts.resetToDefaults();
-		rootEntity.addComponentByType(TextContextStorage, ts);
-	}
+    function textStyles() {
+        // var font = "Assets/fonts/robo.fnt";
+        // fui.addBmFont("", font); // todo
+        var ts = fui.textStyles;
+        ts.newStyle("small-text")
+            .withSize(sfr, .07)
+            .withPadding(horizontal, sfr, 0.1)
+            .withAlign(vertical, Center)
+            .build();
+        ts.resetToDefaults();
+        ts.newStyle("center").withAlign(horizontal, Center).build();
+        ts.newStyle(TextContextBuilder.DEFAULT_STYLE).withAlign(horizontal, Center).build();
+        ts.newStyle("fit")
+            .withSize(pfr, .5)
+            .withAlign(horizontal, Forward)
+            .withAlign(vertical, Backward)
+            .withPadding(horizontal, pfr, 0.33)
+            .withPadding(vertical, pfr, 0.33)
+            .build();
+        rootEntity.addComponent(ts.getStyle("fit"));
+        ts.resetToDefaults();
+        rootEntity.addComponentByType(TextContextStorage, ts);
+    }
 
-	function dkitDefaultStyles() {
-		BaseDkit.inject(fui);
-		var e = rootEntity;
-		var props = e.getOrCreate(PropStorage, () -> new CascadeProps<String>(null, "root-props"));
-		// props.set(Dkit.TEXT_STYLE, "small-text");
+    function dkitDefaultStyles() {
+        BaseDkit.inject(fui);
+        var e = rootEntity;
+        var props = e.getOrCreate(PropStorage, () -> new CascadeProps<String>(null, "root-props"));
+        // props.set(Dkit.TEXT_STYLE, "small-text");
 
-		var distributer = new al.layouts.Padding(new FractionSize(.25), new PortionLayout(Center, new FixedSize(0.1)));
-		var contLayouts = e.getComponent(ContainerStyler);
-		contLayouts.reg(GuiStyles.L_HOR_CARDS, distributer, WholefillLayout.instance);
-		contLayouts.reg(GuiStyles.L_VERT_BUTTONS, WholefillLayout.instance, distributer);
-		// e.addComponent(contLayouts);
-	}
+        var distributer = new al.layouts.Padding(new FractionSize(.25), new PortionLayout(Center, new FixedSize(0.1)));
+        var contLayouts = e.getComponent(ContainerStyler);
+        contLayouts.reg(GuiStyles.L_HOR_CARDS, distributer, WholefillLayout.instance);
+        contLayouts.reg(GuiStyles.L_VERT_BUTTONS, WholefillLayout.instance, distributer);
+        // e.addComponent(contLayouts);
+    }
 }
